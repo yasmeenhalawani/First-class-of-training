5b5013c7adffc5a65b8f240693295f67a43d4b3e
yasmeenhalawani <yasmeenhalawani@hotmail.com>
2023-04-01 13:12:47 -0500

    Message

---
+++ info.py
@@ -0,0 +1,15 @@
+
+#What are complex numbers? 
+# Complex numbers consist of two parts, real and imaginary part.
+
+# root of -1 in python represented by a letter j
+#To create a complex number
+c = 2 + 4j
+
+# THis would be a complex number
+
+# To get the imaginary part of this variable
+print(c.imag) # -> 4
+
+# TO get the real part of this number 
+print(c.real) # -> 2
